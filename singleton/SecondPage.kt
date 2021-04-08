144982eff64c23ca066c328deee811fae6d5b77b
dltmdrb1996 <65164183+dltmdrb1996@users.noreply.github.com>
2021-04-08 21:25:59 +0900

    Delete singleton directory

--- singleton/SecondPage.kt
+++
@@ -1,9 +0,0 @@
-package singleton
-
-class SecondPage{
-    var settings = Settings
-    fun setAndPrintSettings(){
-        println("${settings.getDarkMode()}  ${settings.getFontSize()}")
-    }
-
-}
